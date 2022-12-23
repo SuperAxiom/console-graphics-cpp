27b6fb0dec2201c675c287680dffb64700d47d5f
SuperAxiom <superaxiom@proton.me>
2022-12-23 23:11:50 +0100

    Now coordinates is transformed from 3d to 2d. Add a scene

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ struct Point3d{
 	float x, y, z;
 };
 
+Point3d createPoint3d(float x, float y, float z){
+	Point3d result;
+
+	result.x = x;
+	result.y = y;
+	result.z = z;
+
+	return result;
+}
 
 // Vector in 3d
 struct Vector3d{
@@ -128,12 +137,71 @@ bool isIntersect(Line3d line, Sphere sp){
 	return true;
 }
 
+// Function cheks if point is on sphere
+bool isOnSphere(const Sphere& sp, const Point3d& point){
+	float sum = (point.x - sp.center.x) * (point.x - sp.center.x);
+	sum += (point.y - sp.center.y) * (point.y - sp.center.y);
+	sum += (point.z - sp.center.z) * (point.z - sp.center.z);
+	sum -= sp.radius * sp.radius;
+
+	if (sum <= 0)
+		return true;
+	return false;
+}
+
+// Plane
+struct Plane{
+	float a, b, c, d;
+};
+
+// Functions creates Plane
+Plane createPlane(float a, float b, float c, float d){
+	Plane result;
+
+	result.a = a;
+	result.b = b;
+	result.c = c;
+	result.d = d;
+
+	return result;
+}
+
+// Function returns point where line intersect plane
+Point3d findIntersection(const Plane& plane, const Line3d& line){
+	float t_num = plane.a * line.point.x;
+	t_num += plane.b * line.point.y;
+	t_num += plane.c * line.point.z;
+	t_num += plane.d;
+
+	float t_denum = plane.a * line.vector.coordinates.x;
+	t_denum += plane.b * line.vector.coordinates.y;
+	t_denum += plane.c * line.vector.coordinates.z;
+
+	float t = -1 * (t_num / t_denum);
+
+	float x = line.vector.coordinates.x * t + line.point.x;
+	float y = line.vector.coordinates.y * t + line.point.y;
+	float z = line.vector.coordinates.z * t + line.point.z;
+
+	return createPoint3d(x, y, z);
+}
+
+
+
+struct ImagePlane{
+	Plane plane;
+	Point3d center;
+};
+
 
 void clearScreen(int ROWS, int COLUMNS){
-	for(int j = 0; j < ROWS; j++)
+	for(int j = 0; j < ROWS - 1; j++)
 		for (int i = 0; i < COLUMNS; i++)
 			cout << ' ';
+	cout << endl;
 }
+
+
 
 // Size of terminal window
 const int COLUMNS = 120;
@@ -146,48 +214,85 @@ int main(){
 	// Surface to draw (contains all symbols)
 	char surface[COLUMNS][ROWS];
 
+	// Init surface as blank
+	for(int i = 0; i < COLUMNS; i++)
+		for(int j = 0; j < ROWS; j++)
+			surface[i][j] = ' ';
+
+	// Scene
+	char scene[COLUMNS][ROWS][ROWS];
+
+	// Init scene as blank
+	for (int i = 0; i < COLUMNS; i++)
+		for (int j = 0; j < ROWS; j++)
+			for (int k = 0; k < ROWS; k++)
+				scene[i][j][k] = ' ';
+
+
 	// Clearing screen
 	clearScreen(ROWS, COLUMNS);
 
-
-	// Init camera
-	Point3d camera;
-	camera.x = -10;
-	camera.y = 0;
-	camera.z = -15;
+	// Creating image plane (console in real world)
+	ImagePlane imgPl;
+	imgPl.plane = createPlane(0, 0, 1, 10);
+	imgPl.center = createPoint3d(0, 0, -10);
 
 	Sphere my_sphere;
 	my_sphere.center.x = 0;
 	my_sphere.center.y = 0;
 	my_sphere.center.z = 0;
-	my_sphere.radius = 8.0;
+	my_sphere.radius = 5.0;
+
+	// Adding sphere to the scene
+	for (int i = 0; i < COLUMNS; i++){
+		for (int j = 0; j < ROWS; j++){
+			for (int k = 0; k < ROWS; k++){
+				float x = (float)i / COLUMNS * 20 - 10;
+				x *= ratio * pixelRatio;
+				float y = (float)j / ROWS * 20 - 10;
+				y *= -1;
+				float z = (float)k / ROWS * 20 - 10;
+				if (isOnSphere(my_sphere, createPoint3d(x, y, z)))
+					scene[i][j][k] = '1';
+			}
+		}
+	}
+
 
 	// Drawing sphere
-	for(int t = 0; t <= 360; t++){
-		for(int i = 0; i < COLUMNS; i++){
-			for(int j = 0; j < ROWS; j++){
-				Point3d point0;
-				point0.x = 0;
-				point0.y = 0;
-				point0.z = -5;
+	for (int t = 0; t <= 1000000000; t++){
+		Point3d camera;
+		camera = createPoint3d(imgPl.center.x, imgPl.center.y, imgPl.center.z - 5);
 
-				Point3d point1;
-				point1.x = (float)i / COLUMNS * 20.0 - 10.0;
-				point1.x *= ratio * pixelRatio;
-				point1.y = (float)j / ROWS * 20.0 - 10.0;
-				point1.y *= -1;
-				point1.z = 5.0;
+		for (int i = 0; i < COLUMNS; i++){
+			for (int j = 0; j < ROWS; j++){
+				for (int k = 0; k < ROWS; k++){
+					if (scene[i][j][k] == '1'){
+						float x = (float)i / COLUMNS * 20 - 10;
+						x *= ratio * pixelRatio;
+						float y = (float)j / ROWS * 20 - 10;
+						y *= -1;
+						float z = (float)k / ROWS * 20 - 10;
 
-				Vector3d vector;
-				vector = createVector(point0, point1);
-				vector = norm(vector);
+						Line3d ray = createLine(camera, createPoint3d(x, y, z));
+						Point3d intersection = findIntersection(imgPl.plane, ray);
+						Point3d imagePoint;
 
-				Line3d ray = createLine(camera, vector);
+						imagePoint.x = intersection.x / (ratio * pixelRatio);
+						imagePoint.x += 10;
+						imagePoint.x *= (float)COLUMNS / 20;
+						imagePoint.y = intersection.y + 10;
+						imagePoint.y *= (float)ROWS / 20;
 
-				if (isIntersect(ray, my_sphere))
-					surface[i][j] = '@';
-				else
-					surface[i][j] = ' ';
+						if (imagePoint.x < COLUMNS && imagePoint.y >= 0){
+							if(imagePoint.y < ROWS && imagePoint.y >= 0){
+								int i = (int)imagePoint.x;
+								int j = (int)imagePoint.y;
+								surface[i][j] = '@';
+							}
+						}
+					}
+				}
 			}
 		}
 
@@ -198,16 +303,8 @@ int main(){
 			}
 		}
 
-		// Move sphere
-		//camera.x = 150 * sin(t);
-		//camera.z = 150 * cos(t);
-		//camera.z -= 1;
-		camera.x += 1;
-		if (camera.x == 11)
-			camera.x = -10;
+		// Move imgage plane
 
-		this_thread::sleep_for(chrono::milliseconds(200));
-		clearScreen(ROWS, COLUMNS);
 	}
 
 	return 0;
